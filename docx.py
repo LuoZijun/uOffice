45c4c04bfeba2ef92fad09cc1ac6611a4d05aaf7
luo <luo@freeweapon.org>
2014-12-26 19:02:46 +0800

    ..

--- docx.py
+++ docx.py
@@ -9,23 +9,6 @@ from lxml import etree
 
 
 #     @staticmethod
-"""
-Open Office XML Format:
-    
-    # row
-    w:p   #  ( w->prefix : p->dom name )  # Shuld add one, 
-             # Values: w:rsidR="00D33BCF" w:rsidRDefault="00EF061D"
-        w:pPr || w:r    
-            w:rPr  || w:t # Text
-
-
-        # prefix: m, oMathPara
-        # prefix: m. oMath
-        ## Document from [MS-ODRAWXML].pdf  PAGE: 34
-        ## cGp, cGpRule, cSp, eqArrPr, mathFont, mathPr, mcJc, mcPr, mPr, oMath, r, rSp, rSpRule
-
-"""
-
 
 class Parse:
     def _init__(self):
@@ -44,6 +27,7 @@ class OpenXML:
     def doms(self):
         # Show XML DOM Nodes ( Object )
         root = document.getroot()
+        
         for dom in self.document.getiterator():
             DOMS.append(dom)
         return DOMS
