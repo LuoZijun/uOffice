d56ce0fa1b568348b152b23474b163d5d6424088
Luo <gnulinux@126.com>
2015-02-06 02:07:30 +0800

    .

--- __init__.py
+++ __init__.py
@@ -9,3 +9,19 @@ sys.path.append( os.path.join(os.path.dirname(os.path.abspath(__file__)), 'lib')
 
 __doc__       = "Document"
 __version__   = "0.0000001"
+
+
+def detect_format(file_content):
+	# 猜测文档类型
+	pass
+
+def read_from_file(file):
+	# 从磁盘中读取 文档
+	pass
+
+def read_from_string(file_content):
+	# 从字符串中读取 文档
+	pass
+
+
+
